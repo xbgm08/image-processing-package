1cbc7223aa01be5d05be82b74e2b0cc384bb343b
xbgm08 <gabrielbraulino@gmail.com>
2025-03-21 22:43:14 -0300

    feat: add setup.py for package configuration and dependencies

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setup import setup, find_packages
+
+with open('README.md', 'r') as f:
+    page_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='image_processing',
+    version='0.0.1',
+    author='Gabriel Martins Braulino',
+    description='Image Processing Package using Skimage',
+    long_description=page_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/xbgm08/image-processing-package',
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.5',
+)
